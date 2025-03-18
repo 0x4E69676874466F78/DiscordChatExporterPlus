bfb985f47d1b3c9afc502658504cbd042d77d571
NightFox <0x4E69676874466F78@users.noreply.github.com>
2025-03-18 14:53:53 +0300

    Update DiscordClient.cs

--- DiscordChatExporter.Core/Discord/DiscordClient.cs
+++ DiscordChatExporter.Core/Discord/DiscordClient.cs
@@ -40,8 +40,13 @@ public class DiscordClient(string token)
                     "Authorization",
                     tokenKind == TokenKind.Bot ? $"Bot {token}" : token
                 );
-                var proxy = new WebProxy { Address = new Uri(Environment.GetEnvironmentVariable("proxy")) };
-                var httpClientHandler = new HttpClientHandler { Proxy = proxy };
+
+
+                var proxyAddress = Environment.GetEnvironmentVariable("proxy");
+                var httpClientHandler = new HttpClientHandler();
+                if (!string.IsNullOrEmpty(proxyAddress)) {
+                    httpClientHandler.Proxy = new WebProxy(new Uri(proxyAddress));
+                }
                 var httpClient = new HttpClient(handler: httpClientHandler, disposeHandler: true);
                 var response = await httpClient.SendAsync(
                     request,
